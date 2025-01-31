c93a3b7d95b9a7ba0519a1b6bb9689d85b6f6e1d
Richard Nguyen <richard@richardhnguyen.com>
2025-01-31 15:41:48 -0800

    feat: add article-card

--- src/components/article-card.tsx
+++ src/components/article-card.tsx
@@ -1,50 +1,79 @@
 import React from "react";
-import {GatsbyImageProps, GatsbyImage as Image} from "gatsby-plugin-image";
+import { GatsbyImageProps, GatsbyImage as Image } from "gatsby-plugin-image";
 import { CalendarIcon, TagIcon } from "@primer/octicons-react";
+import { Link } from "gatsby";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 type ArticleCardProps = {
-  title: string;
-  description: string;
-  tag: string;
-  date: string;
-  url: string;
-  image: GatsbyImageProps["image"];
+  frontmatter:
+    | (Omit<Queries.MdxFrontmatter, "thumbnail"> & {
+        thumbnail: {
+          childImageSharp: {
+            gatsbyImageData: GatsbyImageProps["image"];
+          } | null;
+        } | null;
+      })
+    | null;
+  fields: {
+    timeToRead: {
+      text: string | null;
+    } | null;
+  } | null;
+  id: Queries.Mdx["id"];
 };
 
 const ArticleCard: React.FC<
   ArticleCardProps & React.HTMLAttributes<HTMLDivElement>
-> = ({ title, description, tag, date, url, image, ...rest }) => {
+> = ({ frontmatter, fields, id, ...rest }) => {
   return (
-    <div
-      className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col h-full"
+    <article
+      className="bg-background hover:bg-accent border border-border rounded-lg shadow-md overflow-hidden flex flex-col h-full transition-colors pointer-events-auto cursor-default"
       {...rest}
     >
       <Image
-        image={image}
-        alt={title}
-        className="w-full h-48 object-cover"
+        image={
+          frontmatter?.thumbnail?.childImageSharp
+            ?.gatsbyImageData as GatsbyImageProps["image"]
+        }
+        alt={frontmatter?.title ?? "Thumbnail"}
+        className="w-full h-40 object-cover"
       />
 
       <div className="p-4 flex flex-col flex-grow">
-        <h3 className="text-xl font-semibold mb-2">{title}</h3>
+        <h3 className="">
+          <Link
+            to={`/blog/${frontmatter?.slug ?? id}`}
+            className={cn(
+              buttonVariants({ variant: "link", size: "lg" }),
+              "px-0 text-xl font-semibold mb-2"
+            )}
+          >
+            {frontmatter?.title ?? "No title"}
+          </Link>
+        </h3>
 
-        <p className="text-gray-600 mb-4 line-clamp-3 flex-grow">
-          {description}
+        <p className="text-slate-400 dark:text-slate-600 mb-4 line-clamp-3 flex-grow">
+          {frontmatter?.description ?? "No description"}
         </p>
 
-        <div className="flex justify-between items-center text-sm text-gray-500 mt-auto">
+        <div className="flex justify-between items-center text-sm text-slate-500 dark:text-slate-500 mt-auto">
           <div className="flex items-center">
-            <TagIcon className="w-4 h-4 mr-1" />
-            <span>{tag}</span>
+            {frontmatter && (
+              <>
+                <TagIcon className="w-4 h-4 mr-1" />
+                <span>{frontmatter.category}</span>
+              </>
+            )}
           </div>
 
           <div className="flex items-center">
             <CalendarIcon className="w-4 h-4 mr-1" />
-            <span>{date}</span>
+            <span>{frontmatter?.date ?? "No Date"}</span>
           </div>
         </div>
       </div>
-    </div>
+    </article>
   );
 };
 
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { graphql, PageProps } from "gatsby";
 
 import { FilterDropdown } from "./filter-dropdown";
 import { SortDropdown } from "./sort-dropdown";
+import ArticleCard from "@/components/article-card";
 
-export default function Blog() {
+type Props = PageProps<Queries.BlogPageQuery>;
+
+export default function Blog(props: Props) {
+  const { data } = props;
+
   return (
     <div className="min-h-screen max-w-3xl mx-auto pr-4 pl-8 mt-8">
       <main className="flex flex-col row-start-2 items-center sm:items-start">
         <div className="text-center w-full">
           <h1 className="text-4xl font-bold">Blog</h1>
 
-          <div className="flex items-center justify-between mt-8">
+          <div className="flex items-center justify-between mt-8 pb-4 mb-4 border-b">
             <h3>All posts</h3>
 
             <div className="flex items-center gap-2">
@@ -19,8 +25,55 @@ export default function Blog() {
             </div>
           </div>
         </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {data.allMdx.nodes.slice(0, 6).map((node) => {
+            const {fields, id, frontmatter} = node;
+
+            return (
+              <ArticleCard key={id} 
+                frontmatter={frontmatter}
+                fields={fields}
+                id={id}
+              />
+            )
+          })}
+        </div>
       </main>
     </div>
   );
 }
 
+export const query = graphql`
+  query BlogPage {
+    allMdx {
+      nodes {
+        id
+
+        fields {
+          timeToRead {
+            text
+          }
+        }
+
+        frontmatter {
+          slug
+          title
+          date(formatString: "MMM Do, YYYY")
+          author
+          description
+          tags
+          category
+          published
+          publishedAt
+
+          thumbnail {
+            childImageSharp {
+              gatsbyImageData(layout: CONSTRAINED, quality: 100)
+            }
+          }
+        }
+      }
+    }
+  }
+`;
