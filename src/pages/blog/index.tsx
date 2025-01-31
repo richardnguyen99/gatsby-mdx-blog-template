30e9e78fdaeccb7d8419d51623fbf343d20806d7
Richard Nguyen <richard@richardhnguyen.com>
2025-01-31 13:40:17 -0800

    feat: add dropdown menu for filter and sort dropdowns in blogs

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 
+import { FilterDropdown } from "./filter-dropdown";
+import { SortDropdown } from "./sort-dropdown";
+
 export default function Blog() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 gap-16 sm:p-20 sm:py-8 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 sm:items-start">
+    <div className="min-h-screen max-w-3xl mx-auto pr-4 pl-8 mt-8">
+      <main className="flex flex-col row-start-2 items-center sm:items-start">
         <div className="text-center w-full">
-          <h1>Blog</h1>
+          <h1 className="text-4xl font-bold">Blog</h1>
+
+          <div className="flex items-center justify-between mt-8">
+            <h3>All posts</h3>
+
+            <div className="flex items-center gap-2">
+              <FilterDropdown />
+              <SortDropdown />
+            </div>
+          </div>
         </div>
       </main>
     </div>
