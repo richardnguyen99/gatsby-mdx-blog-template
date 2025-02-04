d6808315f576921cabc8d49e8758e212be14c4d5
Richard Nguyen <richard@richardhnguyen.com>
2025-02-04 13:29:41 -0800

    deploy: add sitemetadata and .npmrc for deploy settings

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,6 +4,8 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `gatsby-mdx-blog-template`,
     siteUrl: `https://www.yourdomain.tld`,
+    description: `A Gatsby blog template using MDX`,
+    xUsername: `@RichardNgu65749`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
---
+++ src/components/seo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SEO: React.FC = () => {
+    return (
+        <></>
+    );
+};
+
+export default SEO;
