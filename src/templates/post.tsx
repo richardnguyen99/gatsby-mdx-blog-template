ecbaa2d83de0a2f66cc0e141adeb19e0c351209b
Richard Nguyen <richard@richardhnguyen.com>
2025-02-08 15:42:59 -0800

    update: update slug post layout and title

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -16,10 +16,12 @@ const PostLayout: React.FC<React.PropsWithChildren<PostLayoutData>> = ({
   children,
 }) => {
   return (
-    <div className="min-h-screen max-w-3xl mx-auto pr-4 pl-8 font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen w-full max-w-3xl mx-auto px-2 lg:px-4 xl:px-8 mt-8">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="w-full text-center">
-          <h1 className="mt-4 text-3xl">{data.mdx?.frontmatter?.title}</h1>
+          <h1 className="text-black dark:text-white text-2xl font-bold lg:text-5xl lg:font-black">
+            {data.mdx?.frontmatter?.title}
+          </h1>
 
           <div className="flex items-center justify-between w-full mt-4 mb-4 pb-4 text-sm text-gray-500 border-b dark:border-slate-800 border-b-gray-200">
             <div>{data.mdx?.fields?.timeToRead?.text}</div>
