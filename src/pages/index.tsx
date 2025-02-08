ca60331363fb2af608eb755edca9d92f8b9da944
Richard Nguyen <richard@richardhnguyen.com>
2025-02-08 13:09:01 -0800

    feat: add suspend to image logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,17 @@ const IndexPage: React.FC = () => {
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 gap-16 sm:p-20 sm:pb-8 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="w-full text-center">
-          <ImageCard src="gatsby-icon" alt="icon" className="[&>img]:w-[64px] [&>img]:h-[64px] [&>img]:m-auto" />
+          <React.Suspense
+            fallback={
+              <div className="rounded-full w-[64px] h-[64px] m-auto"></div>
+            }
+          >
+            <ImageCard
+              src="gatsby-icon"
+              alt="icon"
+              className="[&>img]:w-[64px] [&>img]:h-[64px] [&>img]:m-auto"
+            />
+          </React.Suspense>
           <h1 className="mt-4 text-3xl">Gatsby Blog with MDX</h1>
         </div>
       </main>
